4f949e3aa8aae36569fa11117a53163099f1f431
codeOverFlow <adrien.bodineau@gmail.com>
2017-06-18 13:03:05 +0200

    refactor uuid function, add init function, add function to parse args

--- expator.sh
+++ expator.sh
@@ -20,53 +20,86 @@
 
 function uuidfunction()
 {
-    local N B T
+   for N in `seq 0 15`
+   do
+      local B=$(( $RANDOM%255 ))
 
-    for (( N=0; N < 16; ++N ))
-    do
-        B=$(( $RANDOM%255 ))
-
-        if (( N == 6 ))
-        then
+      case N in
+         6)
             printf '4%x' $(( B%15 ))
-        elif (( N == 8 ))
-        then
+            ;;
+         8)
             local C='89ab'
             printf '%c%x' ${C:$(( $RANDOM%${#C} )):1} $(( B%15 ))
-        else
+            ;;
+         *)
             printf '%02x' $B
-        fi
+            ;;
+      esac
 
-        for T in 3 5 7 9
-        do
-            if (( T == N ))
-            then
-                printf '-'
-                break
-            fi
-        done
-    done
-
-    echo
+      for T in `seq 3 2 9`
+      do
+         if ((T == N))
+         then
+            printf '-'
+            break
+         fi
+      done
+   done
 }
 
-ID=`uuidfunction`
-DATE=`date +%Y-%m-%d`
-if test $# -eq 1
-then 
-	DIR=$DATE"_"$1"_"$ID
-	if [ -d "$DIR" ] 
-	then 
-		echo ERROR: Directory $DIR already exists. $jar_file.  1>&2
-    		exit 1
-	else 
-		mkdir $DIR
-		mkdir $DIR/notes
-		mkdir $DIR/data
-		mkdir $DIR/scripts
-		mkdir $DIR/results
-	fi
+function init()
+{
+   ID=`uuidfunction`
+   DATE=`date +%Y-%m-%d`
+   if [ $# -eq 1 ]
+   then 
+      DIR=$DATE"_"$1"_"$ID
+      if [ -d "$DIR" ] 
+      then 
+         echo ERROR: Directory $DIR already exists. $jar_file.  1>&2
+         exit 1
+      else 
+         mkdir $DIR
+         mkdir $DIR/notes
+         mkdir $DIR/data
+         mkdir $DIR/scripts
+         mkdir $DIR/results
+      fi
+   fi
+}
 
-fi
+function parse_args()
+{
+   local EXP_NAME
+   while [[ $# -gt 0 ]]
+   do
+      key="$1"
+      case $key in
+         init)
+            if [ -z $2 ]
+            then
+               EXP_NAME="newExp"
+               shift
+            else
+               EXP_NAME=$2
+               shift
+               shift
+            fi
+            init "${EXP_NAME}"
+            ;;
+         run)
+            shift # past argument
+            $*
+            break
+            ;;
+         *)
+            echo "Unknown option."
+            ;;
+      esac
+      shift # past argument or value
+   done
+}
 
-
+# --- MAIN ---
+parse_args $*
